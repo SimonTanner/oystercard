b0e263b4e0a881eff16a3f43bc6f6fcf1c3c78ef
Paula <pmuldoon@umich.edu>
2017-04-21 17:17:38 +0100

    Paula 1717

--- lib/journey.rb
+++ lib/journey.rb
@@ -6,29 +6,26 @@ class Journey
   MINFARE = 1
 
   def initialize
+    # @entry_station = entry_station
     @journeys = []
   end
 
-  def entry_station(entry_station)
-    @journeys << {:entry_station => entry_station}
+  def entry_station(entry_station = nil)
+    @journey << [:entry_station => entry_station]
   end
-  
-  def exit_station(exit_station)
-    @journeys[-1][:exit_station] = exit_station
+
+  def exit_station(exit_station = nil)
+    @journey.push([:exit_station = exit_station])
   end
-  
+
   def calc_fare
-    
+
   end
 
   def in_journey?
     !!entry_station
   end
 
-  private
 
-  def deduct(amount)
-    @balance -= amount
-  end
 
 end
--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,9 +1,9 @@
 require_relative 'journey'
 
 class Oystercard
-  
+
   attr_reader :journey
-  
+
   MAXBALANCE = 90
   MINBALANCE = 1
 
@@ -18,14 +18,26 @@ class Oystercard
     raise "You cannot exceed a balance of #{MAXBALANCE}" if @balance + amount > MAXBALANCE
     @balance += amount
   end
-  
+
   def touch_in(entry_station)
     fail "Your balance is below #{MINBALANCE} so you cannot travel" if @balance < MINBALANCE
     @journey.entry_station(entry_station)
+      if !@journey.complete?
+        penalty charge
+      end
   end
 
   def touch_out(exit_station)
     @journey.exit_station(exit_station)
+      if @journey.complete? deduct(fare)
+      else penalty charge
+      end
+  end
+
+  private
+
+  def deduct(amount)
+    @balance -= amount
   end
 
 end
